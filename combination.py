dbae803fc12f0389e34cb43cd8f6e677234e5c90
Gireesh Puthumana <Gireesh.Puthumana@lntinfotech.com>
2017-08-18 00:44:00 +0530

    coding standards added

--- combination.py
+++ combination.py
@@ -1,5 +1,6 @@
 #import pdb
 
+
 def comb_fn (str, start, end):
     # pdb.set_trace()
     if start == end-1:
@@ -10,12 +11,14 @@ def comb_fn (str, start, end):
         for i in range(start, end+1):
             comb_fn(move_to_first(str, i), start+1, end)
 
+
 def swap (s, start, end):
     l = len(s)
-    if l <= start-1 or start >= end or l<=end:
+    if any([l <= start-1, start >= end, l <= end]):
         return s
     else:
         return s[:start] + s[end] + s[start+1:end] + s[start] + s[end+1:]
+
 
 def move_to_first (s, index):
     return s[index] + s[:index] + s[index+1:]
