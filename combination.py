1a98bf39d90a3554d8e0d70179b951d0b82dd904
Gireesh Puthumana <Gireesh.Puthumana@lntinfotech.com>
2017-08-18 00:30:31 +0530

    'firstcommit'

---
+++ combination.py
@@ -0,0 +1,24 @@
+import Lib.textwrap as t
+import math
+import JoinStrings as j
+import pdb
+
+def comb_fn (str, start, end):
+    # pdb.set_trace()
+    if start == end-1:
+        print (str)
+        str = swap(str, start, end)
+        print (str)
+    else:
+        for i in range(start, end+1):
+            comb_fn(move_to_first(str, i), start+1, end)
+
+def swap (s, start, end):
+    l = len(s)
+    if l <= start-1 or start >= end or l<=end:
+        return s
+    else:
+        return s[:start] + s[end] + s[start+1:end] + s[start] + s[end+1:]
+
+def move_to_first (s, index):
+    return s[index] + s[:index] + s[index+1:]
---
+++ driver.py
@@ -0,0 +1,5 @@
+import combination as comb
+
+input_str = input('Enter the string: ')
+
+comb.comb_fn(input_str, 0, len(input_str)-1)
